d1fedb092b28a320a128f2a654b25deb1207710b
Burak Ozgul <brk_ozgul@hotmail.com>
2017-10-17 02:24:44 +0300

    Docker UI Connection

---
+++ ConnectionWithSpark/src/Main.java
@@ -0,0 +1,69 @@
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String erors = "";
+        int mean = 0;
+        ArrayList<String> strAll = new ArrayList<String>();
+        String host = "127.0.0.1";
+        String user = "root";
+        String password = "burak123";
+        String command1 = "export SPARK_MAJOR_VERSION=2; spark-submit --class rf --jars /root/rfPredictProject.jar";
+
+        String tempstr = "";
+        StringBuilder sb = new StringBuilder("");
+        try {
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, 2222);
+            session.setPassword(password);
+            session.setConfig(config);
+            session.connect();
+            System.out.println("Connected");
+
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec) channel).setCommand(command1);
+
+            channel.setInputStream(null);
+            ((ChannelExec) channel).setErrStream(System.err);
+
+            InputStream in = channel.getInputStream();
+            channel.connect();
+
+            byte[] tmp = new byte[1024];
+            while (true) {
+                while (in.available() > 0) {
+                    int i = in.read(tmp, 0, 1024);
+                    if (i < 0) break;
+                    //System.out.print(new String(tmp, 0, i));
+                    String temp = new String(tmp, 0, i);
+                    sb.append(temp);
+
+                }
+                if (channel.isClosed()) {
+                    System.out.println("exit-status: " + channel.getExitStatus());
+                    break;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ee) {
+                }
+            }
+            channel.disconnect();
+            session.disconnect();
+            System.out.println("DONE");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.print(sb.toString());
+      
+    }
+}
